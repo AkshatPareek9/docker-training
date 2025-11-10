0cf49a004bcddbdbcd5e3a6668f90fad0f66ae38
Akshat Pareek <62641746+AkshatPareek9@users.noreply.github.com>
2025-11-11 04:24:47 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker login  
+docker pull hello-world  
+docker images  
+docker run hello-world  
+docker ps  
+
+docker pull mysql  
+docker images  
+docker run -d -e MYSQL_ROOT_PASSWORD=root mysql  
+docker ps -a  
+
+docker stop <container_id>  
+docker ps -a  
+docker start <container_id>  
